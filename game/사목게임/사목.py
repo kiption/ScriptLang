36f6f90880ee7fb5eb44a291e4cf2cb25f703298
kiption <89969740+kiption@users.noreply.github.com>
2022-05-14 20:37:24 +0900

    # GUI + Samok

--- game/사목게임/사목.py
+++ game/사목게임/사목.py
@@ -1,48 +1,99 @@
 from tkinter import*
 
-matrix = []
-def drawBoard():
-    for i in range(6):
-        for j in range(7):
-            print('ㅣ',matrix[i][j],' ',end='')
-        print('ㅣ')
-    print("--------------------------")
+class Cell:
+    def __init__(self):
+        window = Tk()
+        window.title("Samok-Game")
+        frame = Frame(window)
+        frame.pack()
+        self.matrix = []
+        self.imageX = PhotoImage(file='game_sub/x.gif')
+        self.imageO = PhotoImage(file='game_sub/o.gif')
+        self.imageE = PhotoImage(file='game_sub/empty.gif')
+        self.turn = True
+        self.done = False
+        for i in range(6):
+            self.matrix.append([])
+            for j in range(7):
+                self.matrix[i].append(Button(frame, image=self.imageE,
+                                          text=' ', command=lambda col=j: self.pressed(col)))
+                self.matrix[i][j].grid(row=i, column=j)
+        frame1 = Frame(window)
+        frame1.pack()
+        self.explain = StringVar()
+        self.explain.set("Player X turn")
+        self.label = Label(frame1, textvariable=self.explain)
+        self.label.pack(side=LEFT)
+        Button(frame1, text='retry', command=self.refresh).pack(side=LEFT)
+        window.mainloop()
 
-def check():
-    for i in range(6):
-        for j in range(7):
+    def findRow(self, col):
+        for row in range(5, -1, -1):  # row: 5->0
+            if self.matrix[row][col]['text'] == ' ':
+                return row
+        return 6
 
-def findRow(col):
-    for row in range(5, -1, -1): # row: 5->0
-        if matrix[row][col] == ' ':
-            return row
-    return 6
+    def pressed(self, col):
+        row = self.findRow(col)
+        if not self.done and self.matrix[row][col]['text']==' ':
+            if self.turn:
+                self.matrix[row][col]['image'] = self.imageX
+                self.matrix[row][col]['text'] ='B'
+            else:
+                self.matrix[row][col]['image'] = self.imageO
+                self.matrix[row][col]['text'] = 'W'
+            self.turn = not self.turn
+            if self.check()!= '':
+                self.done = True
+                self.explain.set('Player'+self.check()+'win')
+            elif self.turn:
+                self.explain.set('Player B turn')
+            else:
+                self.explain.set('Player W turn')
 
-def main():
-    for i in range(6):
-        matrix.append([])
-        for j in range(7):
-            matrix[i].append(' ')
+    def refresh(self):
+        for i in range(6):
+            for j in range(7):
+                self.matrix[i][j]['image'] = self.imageE
+                self.matrix[i][j]['text'] = ' '
+        self.done = False
+        self.explain.set("Player B turn")
+        self.turn = True
 
-    drawBoard()
-    turn = True
-    while(True):
-        if turn:
-            col = eval(input("열 0~6사이 하얀돌을 입력하세요: "))
+    def check(self):
+        #row Check
+        for i in range(6):
+            for j in range(4):  # 4개 판별.
+                Player = self.matrix[i][j]['text']
+                if Player != ' ' and Player == self.matrix[i][j + 1]['text'] \
+                        and Player == self.matrix[i][j + 2]['text'] and Player == self.matrix[i][j + 3]['text']:
+                    return Player
 
-        else:
-            col = eval(input("열 0~6사이 겅은돌을 입력하세요: "))
+        # col Check
+        for i in range(3):
+            for j in range(7):
+                Player = self.matrix[i][j]['text']
+                if Player != ' ' and Player == self.matrix[i + 1][j]['text'] \
+                        and Player == self.matrix[i + 2][j]['text'] and Player == self.matrix[i + 3][j]['text']:
+                    return Player
 
-        row = findRow(col) # 들어갈 행 구하기
-        if turn:
-            matrix[row][col] = 'W'
-        else:
-            matrix[row][col] = 'B'
-        drawBoard()
-        Player = check()
-        if Player !='':
-            print("플레이어", Player, "가 이겼습니다")
-            break
-        turn = not turn
+            # cross Check (left->right)
+        for i in range(3):
+            for j in range(4):  # col 0~3
+                Player = self.matrix[i][j]['text']
+                if Player != ' ' and Player == self.matrix[i + 1][j + 1]['text'] \
+                        and Player == self.matrix[i + 2][j + 2]['text'] and Player == self.matrix[i + 3][j + 3]['text']:
+                    print("대각선 좌에서 우", i, j)
+                    return Player
 
-main()
+            # cross Check (right->left)
+        for i in range(3):
+            for j in range(3, 7):  # col 3~6
+                Player = self.matrix[i][j]['text']
+                if Player != ' ' and Player == self.matrix[i + 1][j - 1]['text'] \
+                        and Player == self.matrix[i + 2][j - 2]['text'] and Player == self.matrix[i + 3][j - 3]['text']:
+                    print("대각선 좌에서 우", i, j)
+                    return Player
+        return ''
+
+Cell()
